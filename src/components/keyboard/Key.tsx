cb5de63b688e6da14f0016ca9fd6fe7cdc2a8bee
Rakela Roshi <rakelaruco@gmail.com>
2022-06-11 19:11:57 +0200

    Add files via upload

---
+++ src/components/keyboard/Key.tsx
@@ -0,0 +1,67 @@
+import { ReactNode } from 'react'
+import classnames from 'classnames'
+import { CharStatus } from '../../lib/statuses'
+import { REVEAL_TIME_MS } from '../../constants/settings'
+import { getStoredIsHighContrastMode } from '../../lib/localStorage'
+import { solution } from '../../lib/words'
+
+type Props = {
+  children?: ReactNode
+  value: string
+  width?: number
+  status?: CharStatus
+  onClick: (value: string) => void
+  isRevealing?: boolean
+}
+
+export const Key = ({
+  children,
+  status,
+  width = 45,
+  value,
+  onClick,
+  isRevealing,
+}: Props) => {
+  const keyDelayMs = REVEAL_TIME_MS * solution.length
+  const isHighContrast = getStoredIsHighContrastMode()
+
+  const classes = classnames(
+    'flex items-center justify-center rounded mx-0.5 text-xs font-bold cursor-pointer select-none dark:text-white',
+    {
+      'transition ease-in-out': isRevealing,
+      'bg-slate-200 dark:bg-slate-600 hover:bg-slate-300 active:bg-slate-400':
+        !status,
+      'bg-slate-400 dark:bg-slate-800 text-white': status === 'absent',
+      'bg-orange-500 hover:bg-orange-600 active:bg-orange-700 text-white':
+        status === 'correct' && isHighContrast,
+      'bg-cyan-500 hover:bg-cyan-600 active:bg-cyan-700 text-white':
+        status === 'present' && isHighContrast,
+      'bg-blue-500 hover:bg-blue-600 active:bg-blue-700 text-white':
+        status === 'correct' && !isHighContrast,
+      'bg-amber-500 hover:bg-amber-600 active:bg-amber-700 text-white':
+        status === 'present' && !isHighContrast,
+    }
+  )
+
+  const styles = {
+    transitionDelay: isRevealing ? `${keyDelayMs}ms` : 'unset',
+    width: `${width}px`,
+    height: '58px',
+  }
+
+  const handleClick: React.MouseEventHandler<HTMLButtonElement> = (event) => {
+    onClick(value)
+    event.currentTarget.blur()
+  }
+
+  return (
+    <button
+      style={styles}
+      aria-label={`${value} ${status}`}
+      className={classes}
+      onClick={handleClick}
+    >
+      {children || value}
+    </button>
+  )
+}
